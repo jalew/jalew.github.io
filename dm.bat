64b5aace3b4200aecd9075a6f9b951a88754500c
jalew <jack.lewandowski@gmail.com>
2019-10-08 20:27:45 +0200

    Update

--- dm.bat
+++
@@ -1,7 +0,0 @@
-SET pass=winter2018
-echo on
-call git init
-call git remote add origin https://github.com/jalew/jalew.github.io.git
-call git add *
-call git commit -m "Update"
-call git push -f https://jalew:6848Jaca44@github.com/jalew/jalew.github.io.git master
